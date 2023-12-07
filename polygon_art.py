7afe46b0b774ddf66340db9f58ceeff80601c08b
Pawat Sarnchawanakit <pawat.sar@ku.th>
2023-12-07 09:23:40 +0700

    OOP builder pattern cuz its cool...

--- polygon_art.py
+++ polygon_art.py
@@ -1,52 +1,88 @@
 import turtle
 import random
 
-def draw_polygon(num_sides, size, orientation, location, color, border_size):
-    turtle.penup()
-    turtle.goto(location[0], location[1])
-    turtle.setheading(orientation)
-    turtle.color(color)
-    turtle.pensize(border_size)
-    turtle.pendown()
-    for _ in range(num_sides):
-        turtle.forward(size)
-        turtle.left(360/num_sides)
-    turtle.penup()
+class Color:
+    def random():
+        return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-def get_new_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+class Polygon:
+    def internal_angle(self, angle):
+        self.__angle = angle;
+        return self;
+    def color(self, col):
+        self.__col = col;
+        return self;
+    def thickness(self, thickness):
+        self.__thickness = thickness;
+        return self;
+    def orientation(self, angle):
+        self.__start_angle = angle;
+        return self;
+    def length(self, length):
+        self.__length = length;
+        return self;
+    def draw(self, x, y):
+        turtle.penup();
+        turtle.goto(x, y)
+        turtle.setheading(self.__start_angle)
+        turtle.color(self.__col)
+        turtle.pensize(self.__thickness)
+        turtle.pendown()
+        for _ in range(360//self.__angle):
+            turtle.forward(self.__length)
+            turtle.left(self.__angle)
+        turtle.penup()
 
 turtle.speed(0)
 turtle.bgcolor('black')
 turtle.tracer(0)
 turtle.colormode(255)
 
-# draw a polygon at a random location, orientation, color, and border line thickness
-num_sides = random.randint(3, 5) # triangle, square, or pentagon
-size = random.randint(50, 150)
-orientation = random.randint(0, 90)
-location = [random.randint(-300, 300), random.randint(-200, 200)]
-color = get_new_color()
-border_size = random.randint(1, 10)
-draw_polygon(num_sides, size, orientation, location, color, border_size)
+# # draw a polygon at a random location, orientation, color, and border line thickness
+# num_sides = random.randint(3, 5) # triangle, square, or pentagon
+# size = random.randint(50, 150)
+# orientation = random.randint(0, 90)
+# location = [random.randint(-300, 300), random.randint(-200, 200)]
+# color = get_new_color()
+# border_size = random.randint(1, 10)
+# draw_polygon(num_sides, size, orientation, location, color, border_size)
 
-# specify a reduction ratio to draw a smaller polygon inside the one above
-reduction_ratio = 0.618
+# # specify a reduction ratio to draw a smaller polygon inside the one above
+# reduction_ratio = 0.618
 
-# reposition the turtle and get a new location
-turtle.penup()
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.left(90)
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.right(90)
-location[0] = turtle.pos()[0]
-location[1] = turtle.pos()[1]
+# # reposition the turtle and get a new location
+# turtle.penup()
+# turtle.forward(size*(1-reduction_ratio)/2)
+# turtle.left(90)
+# turtle.forward(size*(1-reduction_ratio)/2)
+# turtle.right(90)
+# location[0] = turtle.pos()[0]
+# location[1] = turtle.pos()[1]
 
-# adjust the size according to the reduction ratio
-size *= reduction_ratio
+# # adjust the size according to the reduction ratio
+# size *= reduction_ratio
 
-# draw the second polygon embedded inside the original 
-draw_polygon(num_sides, size, orientation, location, color, border_size)
+# # draw the second polygon embedded inside the original 
+# draw_polygon(num_sides, size, orientation, location, color, border_size)
 
-# hold the window; close it by clicking the window close 'x' mark
+# # hold the window; close it by clicking the window close 'x' mark
+
+art_num = input("Which art do you want to generate? Enter a number between 1 to 8, inclusive: ")
+if art_num == '1':
+    for _ in range(30):
+        Polygon().color(Color.random()) \
+            .thickness(random.randint(1, 10)) \
+            .orientation(random.randint(0, 90)) \
+            .internal_angle(120) \
+            .length(random.randint(50, 150)) \
+            .draw(random.randint(-300, 300), random.randint(-200, 200));
+elif art_num == '2':
+    for _ in range(30):
+        Polygon().color(Color.random()) \
+            .thickness(random.randint(1, 10)) \
+            .orientation(random.randint(0, 90)) \
+            .internal_angle(90) \
+            .length(random.randint(50, 150)) \
+            .draw(random.randint(-300, 300), random.randint(-200, 200));
 turtle.done()
+
